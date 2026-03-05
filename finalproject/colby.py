62b56ab0183893647e92f12c68ae150d52024cda
TropicalKnot74 <comichaelprice74@gmail.com>
2026-03-05 13:56:27 -0800

    Update colby.py

--- finalproject/colby.py
+++ finalproject/colby.py
@@ -1,26 +1,6 @@
 #Front Menu
 
 choice = float(input("Choose what you want to see--\n\nType 1 <--Your status of immigration \nType 2 <--For legal assistance in your surrounding area \nType 3 <-- For food assist., financial assist., religous, and therapy \nType 4 <-- For advocacy and representation\n"))
-  
-if choice == 1:
-  print("status")
-  
-elif choice == 2:
-  print("legal")
-  
-elif choice == 3:
-  print("assistance")
-  
-elif choice == 4:
-  print("representation")
-  
-else:
-  print("~ only options: 1-4 ~")
-
-
-#Front Menu
-
-choice = float(input("Choose what you want to see--\n\nType 1 <--Your status of immigration \nType 2 <--For legal assistance in your surrounding area \nType 3 <-- For food assist., financial assist., religous, and therapy \nType 4 <-- For advocacy and representation\n"))
 while choice >0 or choice <5:
     
     if choice == 1:
