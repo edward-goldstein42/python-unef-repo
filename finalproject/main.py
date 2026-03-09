b2bc4e1a9e75f17c05a0df445c5f6c511172ba00
edward-goldstein42 <egoldstein42@gmail.com>
2026-03-08 17:39:53 -0700

    Update main.py

--- finalproject/main.py
+++ finalproject/main.py
@@ -1,11 +1,25 @@
-"""
-Final Project Main Program
-Team collaboration file
-"""
+
+#Final Project Main Program
+#Team collaboration file
+#files - main.py, dictionary_data.py, food_menu.py, income_eligibility.py, and eligibility.py
+from food_menu import run_menu
 
 def main():
-    print("Final Project Program Starting")
-
+    run_menu()
 
 if __name__ == "__main__":
     main()
+
+"""
+Main once components are completed
+
+from eligibility import immigration_status
+from main_menu import show_menu
+
+def main():
+    immigration_eligibility = immigration_status()
+    show_menu(immigration_eligibility)
+
+if __name__ == "__main__":
+    main()
+"""
